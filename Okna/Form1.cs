ab0a799430a6e3e681a96b6cbba2845a0d676615
slawek19926 <slawek19926@gmail.com>
2018-12-12 01:31:31 +0100

    zmiany w updacie

--- Okna/Form1.cs
+++ Okna/Form1.cs
@@ -222,20 +222,6 @@ namespace Okna
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
-            AutoUpdater.Start("C:/back/version.xml");
-
-            System.Timers.Timer timer = new System.Timers.Timer
-            {
-                Interval = 2 * 60 * 1000,
-                SynchronizingObject = this
-            };
-            timer.Elapsed += delegate
-            {
-                AutoUpdater.Start("https://github.com/slawek19926/Okna/blob/1.5.4.0/version.xml");
-            };
-            timer.Start();
-
             var MyIni = new INIFile("WektorSettings.ini");
             logged.Text = MyIni.Read("user", "logged");
             
